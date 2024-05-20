35721aa77a67f549298bdb237c66e3185fad6aeb
locnv_GSV_Development.dept <locnv@glory-software.vn>
2024-05-20 13:56:11 +0700

    feat: add Material lib

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,58 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { EmpAddEditComponent } from './employee/emp-add-edit/emp-add-edit.component';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSelectModule } from '@angular/material/select';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+
+const MaterialModule = [
+  MatToolbarModule,
+  MatIconModule,
+  MatButtonModule,
+  MatDialogModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatDatepickerModule,
+  MatNativeDateModule,
+  MatRadioModule,
+  MatSelectModule,
+  ReactiveFormsModule,
+  HttpClientModule,
+  MatTableModule,
+  MatPaginatorModule,
+  MatSortModule,
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmpAddEditComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ...MaterialModule
   ],
+  exports: [
+    BrowserModule,
+    AppRoutingModule,
+    ...MaterialModule
+  ],
+
   providers: [
     provideAnimationsAsync()
   ],
